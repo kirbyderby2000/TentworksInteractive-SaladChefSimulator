8929a6dfc7e0e18a517488b31921cecdf4c2304d
Bryan <kirbyderby2000@gmail.com>
2020-11-14 08:37:56 -0800

    Building the player controller. The player controller will be state-driven and each state will derive from this base PlayerState class created. The PlayerState class will handle the player input received from a player controller class. + Added the base abstract PlayerState class; states must derive from this class + Added a PlayerInput scruct which houses all the player input data. The PlayerState will be responsible for handing the player's input

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base abstract class used to handle the player controller state
+/// </summary>
+public abstract class PlayerState
+{
+
+    /// <summary>
+    /// Method used to handle the player input data
+    /// </summary>
+    /// <param name="input"></param>
+    public abstract void HandlePlayerInput(PlayerInput input);
+}
+
+/// <summary>
+/// Basic struct used to house player input data
+/// </summary>
+public struct PlayerInput
+{
+    /// <summary>
+    /// The player's movement input
+    /// </summary>
+    public Vector2 moveInput;
+
+    /// <summary>
+    /// Whether or not the pick-up / drop button has been pressed
+    /// </summary>
+    public bool pickUpDropPressed;
+
+    /// <summary>
+    /// Whether or not the action / interact button has been pressed
+    /// </summary>
+    public bool actionButtonPressed;
+
+    /// <summary>
+    /// Whether or not the action / interact button is being held down
+    /// </summary>
+    public bool actionButonHeld;
+}
