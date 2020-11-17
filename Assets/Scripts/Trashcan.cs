9dd25275623587b471f1bfaf6c7779e78e811614
Bryan <kirbyderby2000@gmail.com>
2020-11-17 15:46:02 -0800

    - Created an interactable trash can prefab using the Trashcan script (Trashcan script inherits from Interactable)
    - Increased the size of the sliced lettuce prefab
    - Slightly adjusted the chopping board's trigger area position

---
+++ Assets/Scripts/Trashcan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashcan : Interactable
+{
+    public override void PlayerDroppedItem(HoldableItem droppedItem, PlayerController playerThatDroppedTheItem)
+    {
+        FoodGameObject droppedFood = droppedItem.GetComponent<FoodGameObject>();
+
+        if(droppedFood != null)
+        {
+            Destroy(droppedItem.gameObject);
+        }
+    }
+}
