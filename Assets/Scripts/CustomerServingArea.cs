146d67fbbbeaee60f4a0e188d08ffec2cb1921b2
Bryan <kirbyderby2000@gmail.com>
2020-11-18 13:32:52 -0800

    - Creating a script for the customer serving area

---
+++ Assets/Scripts/CustomerServingArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerServingArea : Interactable
+{
+    [SerializeField] Transform servingAreaParent;
+
+    public override void PlayerDroppedItem(HoldableItem droppedItem, PlayerController playerThatDroppedTheItem)
+    {
+        if (servingAreaParent.transform.childCount >= 1)
+            return;
+        droppedItem.ToggleRigidBodyKinematic(true);
+        droppedItem.transform.SetParent(servingAreaParent);
+        droppedItem.transform.position = servingAreaParent.transform.position;
+    }
+}
