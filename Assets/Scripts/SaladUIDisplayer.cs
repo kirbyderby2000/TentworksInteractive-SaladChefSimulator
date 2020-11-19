e036aa6ae6aadb0fe254d8df723212f1d91f203c
Bryan <kirbyderby2000@gmail.com>
2020-11-19 08:49:10 -0800

    - Creating a salad UI displayer (this will eventually be used by both the customer and salad bowls)

---
+++ Assets/Scripts/SaladUIDisplayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaladUIDisplayer : MonoBehaviour
+{
+    [SerializeField] List<UnityEngine.UI.Image> spriteImages;
+    [SerializeField] CanvasGroup saladUICanvasGroup;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void ToggleSaladUI(bool toggle)
+    {
+        saladUICanvasGroup.alpha = toggle ? 1.0f : 0.0f;
+    }
+
+    public void UpdateSaladIngredientsUI(List<Ingredient> ingredients)
+    {
+        if(ingredients.Count == 0)
+        {
+            ToggleSaladUI(false);
+        }
+        else
+        {
+            for (int i = 0; i < spriteImages.Count; i++)
+            {
+                if(ingredients.Count < i)
+                {
+                    spriteImages[i].sprite = ingredients[i].IngredientSprite;
+                    spriteImages[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    spriteImages[i].gameObject.SetActive(false);
+                }
+            }
+
+
+            ToggleSaladUI(true);
+        }
+    }
+}
