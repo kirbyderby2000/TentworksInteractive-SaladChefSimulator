086706525a5775f38821e566693f6e26d5e026cd
Bryan <kirbyderby2000@gmail.com>
2020-11-18 19:14:54 -0800

    - Added power-ups to reward the player - Exposed a method on the PlayerController that allows the player move speed modifier to be changed

---
+++ Assets/Scripts/PowerUpSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpeed : MonoBehaviour
+{
+    [SerializeField] float duration = 10.0f;
+
+    [Min(0.0f)]
+    [SerializeField] float speedModAMT = 0.25f;
+
+
+    public void ActivatePowerUp(PlayerController player)
+    {
+        StartCoroutine(PowerUpCoroutine(player));
+    }
+
+    IEnumerator PowerUpCoroutine(PlayerController player)
+    {
+        player.AddToMoveSpeedModifier(speedModAMT);
+        yield return new WaitForSeconds(duration);
+        player.AddToMoveSpeedModifier(-speedModAMT);
+        yield return null;
+        Destroy(this.gameObject);
+    }
+}
