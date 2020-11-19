0209620fe4723d1e959c7b6ab8089754ca11ea3d
Bryan <kirbyderby2000@gmail.com>
2020-11-19 10:33:10 -0800

    - Added a UI for showing the ingredients inside a salad bowl (the bowl carried around by the players) - Had to change the script execution order so the MainCamera script is executed before any other script, the main camera singleton needs to be assigned before any UI can access it

---
+++ Assets/Scripts/SaladBowlUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaladBowlUI : MonoBehaviour
+{
+    [SerializeField] SaladLocator saladBowl;
+
+    [SerializeField] SaladUIDisplayer saladIngredientsUI;
+
+
+    private void Awake()
+    {
+        saladBowl.SaladBowlAttached.OnSaladIngredientsChanged.AddListener(OnSaladBowlIngredientsChanged);
+        OnSaladBowlIngredientsChanged(saladBowl.SaladBowlAttached);
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position = MainCamera.CameraInstance.WorldToScreenPoint(saladBowl.transform.position);
+    }
+
+
+    private void OnSaladBowlIngredientsChanged(SaladBowl salad)
+    {
+        saladIngredientsUI.UpdateSaladIngredientsUI(salad.SaladIngredients);
+        UpdatePosition();
+        this.gameObject.SetActive(salad.SaladIngredients.Count > 0);
+    }
+
+}
